244ca12cae2c4a87fa159a2cf93af37074dc494e
ema <eusan91@hotmail.com>
2017-04-21 21:48:36 -0600

    creation of a detail working, missing persistence

--- ManejoGastosMensuales/app/src/main/java/com/santamaria/manejogastosmensuales/Activities/CategoryDetailed.java
+++ ManejoGastosMensuales/app/src/main/java/com/santamaria/manejogastosmensuales/Activities/CategoryDetailed.java
@@ -1,10 +1,14 @@
 package com.santamaria.manejogastosmensuales.Activities;
 
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import com.santamaria.manejogastosmensuales.Adapter.CategoryDetailedAdapter;
 import com.santamaria.manejogastosmensuales.Domain.CategoryDetail;
@@ -13,12 +17,19 @@ import com.santamaria.manejogastosmensuales.R;
 import java.util.LinkedList;
 import java.util.List;
 
-public class CategoryDetailed extends AppCompatActivity {
+public class CategoryDetailed extends AppCompatActivity implements View.OnClickListener {
 
     private ListView listViewDetail;
     private Toolbar myToolbar = null;
     CategoryDetailedAdapter adapter;
     List<CategoryDetail> categoryDetailList;
+
+    // variables for AlertDialog
+    private EditText detailInput;
+    private EditText amountInput;
+    private TextView okButton;
+    private TextView cancelButton;
+    private AlertDialog alertDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,10 +65,59 @@ public class CategoryDetailed extends AppCompatActivity {
 
     public void fabAddNewDetail(View view) {
 
-        CategoryDetail categoryDetail = new CategoryDetail("detalle" + (adapter.getCount()+1), ((adapter.getCount()+1)*100));
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Create new category detail");
+
+        View viewInflated = LayoutInflater.from(this).inflate(R.layout.dialog_create_detail, null);
+        builder.setView(viewInflated);
+
+        detailInput = (EditText) viewInflated.findViewById(R.id.newDetail);
+        amountInput = (EditText) viewInflated.findViewById(R.id.newAmount);
+        okButton = (TextView) viewInflated.findViewById(R.id.okButton);
+        okButton.setOnClickListener(this);
+        cancelButton = (TextView) viewInflated.findViewById(R.id.cancelButton);
+        cancelButton.setOnClickListener(this);
+
+        alertDialog = builder.create();
+        alertDialog.show();
+
+    }
+
+
+    private void addNewCategoryDetail(CategoryDetail categoryDetail) {
         categoryDetailList.add(categoryDetail);
-
         adapter.notifyDataSetChanged();
+    }
 
+    @Override
+    public void onClick(View view) {
+        if (view.getId() == R.id.okButton) {
+
+            String detail = detailInput.getText().toString().trim();
+
+            if (detail.isEmpty()) {
+                detailInput.setError("Detail name should not be blank");
+
+            } else {
+
+                String amount = amountInput.getText().toString().trim();
+
+                if (amount.isEmpty()) {
+                    amountInput.setError("Amount should not be blank");
+
+                } else {
+
+                    float amountF = Float.parseFloat(amount);
+
+                    CategoryDetail categoryDetail = new CategoryDetail(detail, amountF);
+                    addNewCategoryDetail(categoryDetail);
+                    alertDialog.dismiss();
+
+                }
+            }
+
+        } else if (view.getId() == R.id.cancelButton){
+            alertDialog.dismiss();
+        }
     }
 }
